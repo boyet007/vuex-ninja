ec7d99a00517932c12fffc4d00c3aaa417c4ce86
Philip Loa <boyet007@gmail.com>
2018-11-25 07:54:34 +0700

    tutorial 1 - 4 using computed properties

--- src/store.js
+++ src/store.js
@@ -5,7 +5,13 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    products: [
+      {name: 'Banana skins', price: 20},
+      {name: 'Shiny Star', price: 40},
+      {name: 'Green Shells', price: 60},
+      {name: 'Red Shells', price: 80},
+      {name: 'buah mangga', price: 70}
+    ]
   },
   mutations: {
 
